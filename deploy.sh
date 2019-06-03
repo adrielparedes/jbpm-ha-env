17f03f7817517e4adc489fe0da103d7168b05e06
Adriel <adriel.paredes@gmail.com>
2019-06-03 13:03:55 -0300

    New helpers

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+WORKSPACE=/data/dev/workspace/redhat/bsig-dev
+DISTRIBUTIONS=kie-wb-distributions/business-central-parent/business-central-distribution-wars
+WEBAPP=kie-wb-distributions/business-central-parent/business-central-webapp
+
+#ARTIFACT=business-central-7.23.0-SNAPSHOT-wildfly14.war
+#ARTIFACT=business-central-7.23.0-SNAPSHOT-eap7-redhat.war
+
+#FROM=$WORKSPACE/$DISTRIBUTIONS/business-central/target/$ARTIFACT
+FROM=$WORKSPACE/$WEBAPP/target/business-central-webapp.war
+
+###### JBPM-WB ######
+#FROM=/data/dev/workspace/redhat/bsig-dev/jbpm-wb/jbpm-wb-showcase/target/jbpm-wb-showcase.war
+
+###### DROOLS-WB ######
+#FROM=/data/dev/workspace/redhat/bsig-dev/drools-wb/drools-wb-webapp/target/drools-wb-webapp.war
+
+###### TO ######
+TO_1=./wildfly/deployments1
+TO_2=./wildfly/deployments2
+
+
+echo "Deploying $ARTIFACT in Wildfly"
+rm -rf $TO_1/*
+cp $FROM $TO_1
+rm -rf $TO_2/*
+cp $FROM $TO_2
