26b362e15f2c67295758e3014d444d38d25cf0ae
ederign <ignatowicz@gmail.com>
2019-06-06 13:25:58 -0400

    OSX NFS

--- nfs-mount.sh
+++ nfs-mount.sh
@@ -2,3 +2,7 @@
 
 sudo mount 127.0.0.1:/data/nfs ./nfs/niogit1
 sudo mount 127.0.0.1:/data/nfs ./nfs/niogit2
+
+#for OSX, you need to add /Users/ederign/nfs/niogit to /etc/exports
+#sudo mount -o rw -t nfs 127.0.0.1:/Users/ederign/nfs/niogit ./nfs/niogit1
+#sudo mount -o rw -t nfs 127.0.0.1:/Users/ederign/nfs/niogit ./nfs/niogit2
