f8d38466b7435795534351801796930f4b5667e3
Adriel <adriel.paredes@gmail.com>
2019-06-20 20:44:26 -0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ DISTRIBUTIONS=kie-wb-distributions/business-central-parent/business-central-dist
 WEBAPP=kie-wb-distributions/business-central-parent/business-central-webapp
 
 #ARTIFACT=business-central-7.23.0-SNAPSHOT-wildfly14.war
-#ARTIFACT=business-central-7.23.0-SNAPSHOT-eap7-redhat.war
+ARTIFACT=business-central-7.24.0-SNAPSHOT-eap7-redhat.war
 
 #FROM=$WORKSPACE/$DISTRIBUTIONS/business-central/target/$ARTIFACT
-#FROM=$WORKSPACE/$WEBAPP/target/business-central-webapp.war
+FROM=$WORKSPACE/$WEBAPP/target/business-central-webapp.war
 
-FROM=/data/dev/workspace/redhat/bsig-dev/drools-wb/drools-wb-webapp/target/drools-wb-webapp.war
+#FROM=/data/dev/workspace/redhat/bsig-dev/drools-wb/drools-wb-webapp/target/drools-wb-webapp.war
 
 ###### JBPM-WB ######
 #FROM=/data/dev/workspace/redhat/bsig-dev/jbpm-wb/jbpm-wb-showcase/target/jbpm-wb-showcase.war
