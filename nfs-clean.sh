3f110e8602e5424734b03766b878be88ef8dd143
Adriel <adriel.paredes@gmail.com>
2019-08-30 16:28:11 -0300

    removed unused replacements in war creation

--- nfs-clean.sh
+++ nfs-clean.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
+<<<<<<< Updated upstream
 rm -rf nfs/shared/.niogit/*
 rm -rf nfs/shared/.niogit/.*
 rm -rf nfs/shared/.kie-wb-playground/*
 rm -rf nfs/shared/.kie-wb-playground/.*
+=======
+rm -rf nfs/niogit1/.niogit/*
+rm -rf nfs/niogit1/.niogit/.*
+rm -rf nfs/not-shared/.niogit/*
+rm -rf nfs/not-shared/.niogit/.*
+rm -rf nfs/not-shared/.kie-wb-playground/*
+rm -rf nfs/not-shared/.kie-wb-playground/.*
+rm -rf nfs/niogit1/.kie-wb-playground/*
+rm -rf nfs/niogit1/.kie-wb-playground/.*
+>>>>>>> Stashed changes
